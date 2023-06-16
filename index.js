4495f9551bde52807e0b8ce7910c8cb867788715
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-06-16 11:41:02 +0400

    parsing request bodies

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>hello</title></head>');
-    res.write('<body><h1>node js</h1></body>');
-    res.write('</html>');
 
-    res.end();
+    const method = req.method;
+
+    if (req.url === '/') {
+        res.write('<html>');
+        res.write('<head><title>message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">submit</button></form></body>');
+        res.write('</html>');
+
+        return res.end();
+    }
+    if (req.url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            console.log(message);
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        res.end();
+    }
 });
 
 
