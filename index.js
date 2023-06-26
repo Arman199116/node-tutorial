680b53a2a0a745ddf9a8967ee1b84835ffcc9e58
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-06-26 11:00:16 +0400

    express router

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    console.log('in another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">ADD Product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>hello from express1</h1>');
-});
 
 app.listen(3000);
 
