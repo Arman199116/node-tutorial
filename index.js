78d62df63ec349fdc5f83b8a1787495da63c8c33
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-06-26 10:47:21 +0400

    parsing incomming requests

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in middleware');
-    next(); // continue nex middleware
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('in another middleware');
-    res.send('<h1>hello from express</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">ADD Product</button></form>');
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>hello from express1</h1>');
+});
 
 app.listen(3000);
+
