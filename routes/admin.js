1dd01cc35a5a0051ce8287ef96f9d376c13b9c3d
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-06-27 10:25:27 +0400

    filtering paths

--- routes/admin.js
+++ routes/admin.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     console.log('in another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">ADD Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"> <button type="submit">ADD Product</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
