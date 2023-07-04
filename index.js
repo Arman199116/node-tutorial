0b329289569e5926eec0890aeaf71c0f13de020c
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-07-04 10:31:25 +0400

    finishing pug template

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const path = require('path');
 
 
 const app = express();
+var cors = require('cors')
+
+app.use(cors());
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -17,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Not found'});
 });
 
 
