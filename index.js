bc6a63c147648f387f2a2a5b95d5dd656339d8a5
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-07-05 11:23:10 +0400

    ejsand layout partials

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
-const expressHbs = require('express-handlebars');
 
 
 const app = express();
@@ -12,12 +11,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.engine('hbs', expressHbs.engine({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  }));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -27,7 +21,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Not found'});
+    res.status(404).render('404', {pageTitle: 'Not found', path: ''});
 });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,6 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
-    console.log(req.body);
     res.render('shop', { 
         prods: products,
         pageTitle: 'Shop',
