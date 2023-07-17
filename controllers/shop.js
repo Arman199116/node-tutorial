858c936a4bdfa2368992596dd9fac430c9d874ae
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-07-17 11:58:18 +0400

    displaying cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,7 +5,7 @@ exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('shop/products-list', {
             prods: products,
-            pageTitle: 'All products',
+            pageTitle: 'All product',
             path: '/products'
         });
     });
@@ -40,9 +40,21 @@ exports.getOrders = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (const product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if (cartProductData) {
+                    cartProducts.push({productData: product, qty: cartProductData.qty});
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        });
     });
 }
 
@@ -50,8 +62,8 @@ exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
     Product.findById(prodId, product => {
         Cart.addProduct(prodId, product.price);
+        res.redirect('/cart');
     })
-    res.redirect('/');
 }
 
 exports.getCheckout = (req, res, next) => {
