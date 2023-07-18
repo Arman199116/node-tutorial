324f331c8eb8316cb0fa1e8d2465dc0c3fb1b859
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-07-18 11:40:44 +0400

    delete cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -66,6 +66,14 @@ exports.postCart = (req, res, next) => {
     })
 }
 
+exports.postCartDelete = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price);
+        res.redirect('/cart');
+    })
+}
+
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
