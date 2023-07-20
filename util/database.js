c5631318d55bd671897396a4a2862c625a5893ab
armanochinyan <arman.ochinyan.im.cs@gmail.com>
2023-07-20 11:24:13 +0400

    defining model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize  = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'goyaArman'
+const sequelize = new Sequelize('node-complete', 'root', 'goyaArman', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
